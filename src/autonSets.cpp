3cfe56d9fc6d57ba82b316a14d90a748fc7bd562
Chen An Hong <cahchenanhong@gmail.com>
2021-02-24 18:23:28 +0800

    fixed and tuned control

--- src/autonSets.cpp
+++ src/autonSets.cpp
@@ -11,34 +11,60 @@ void scoreTower() {
   intake(0);
 }
 
-
-void skillsRoute() {
+void scoreFinal() {
   intake(127);
-  baseMove(47);
-  waitBase(1000);
-  delay(100);
-
-  baseMove(-16);
-  waitBase(500);
-  delay(100);
-  intake(0);
-  baseTurn(-63);
-  waitBase(1000);
-  delay(100);
-  baseMove(18);
-  waitBase(1000);
-
-  powerBase(60, 60);
-  intake(127);
+  timerBase(50, 50, 700);
+  powerBase(0, 0);
   shootBall();
   waitShooter();
-  delay(200);
-  shootBall();
-  waitShooter();
+  delay(100);
   waitLoaded();
   intake(0);
-  unPauseBase();
-  baseMove(-10);
-  waitBase(1000);
-  delay(100);
+  timerBase(-80, -80, 500);
+}
+
+void skillsRoute() {
+  intake(50);
+  baseTurn(90);
+  waitBase(100000);
+  intake(0);
+  delay(1000);
+  // // intake 2 balls
+  // intake(127);
+  // baseMove(47);
+  // waitBase(1000);
+  // // delay(100);
+  // baseMove(-16);
+  // waitBase(500);
+  // delay(100);
+  // intake(0);
+  //
+  // // turn to tower 1
+  // baseTurn(-63);
+  // waitBase(1000);
+  // // delay(100);
+  // baseMove(18);
+  // waitBase(1000);
+  //
+  // // score tower 1
+  // powerBase(60, 60);
+  // intake(127);
+  // shootBall();
+  // waitShooter();
+  // delay(100);
+  // shootBall();
+  // waitShooter();
+  // waitLoaded();
+  // intake(-127);
+  // unPauseBase();
+  //
+  //
+  // baseMove(-10);
+  // waitBase(1000);
+  // baseTurn(-20);
+  // waitBase(1000);
+  // baseMove(-58);
+  // waitBase(2000);
+  // baseTurn(60);
+  // waitBase(1000);
 }
